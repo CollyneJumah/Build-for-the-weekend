93a1d3fba1fa0714262cc50420c5a69100fa3bab
CollyneJumah <collynejumah2010@gmail.com>
2022-08-15 14:39:56 +0300

    FIX project Item on hover

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,10 +7,10 @@ export const Contact = () => {
             <p className='uppercase text-xl tracking-widest text-[#5651e5] font-semibold'>{`Let's Talk`}</p>
             <h2 className='py-4'>Drop your message</h2>
             <div className="grid lg:grid-cols-5 gap-8">
-                <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+                <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4 hover:cursor-pointer">
                     <div className="lg:p-4 h-full">
                         <div>
-                            <img src="" className='rounded-xl hover:scale-105 ease-in duration-300' alt="" />
+                            <img src="https://media.istockphoto.com/photos/closeup-image-of-male-hands-using-smartphone-with-icon-telephone-picture-id1168945108?k=20&m=1168945108&s=612x612&w=0&h=MqKJyjMB1NJ33aRB9kjhxqP_GbPmEVB11saJi9sCuwM=" className='rounded-xl hover:scale-105 ease-in duration-300' alt="" />
                         </div>
                     </div>
                 </div>
