f2660980f93cc54ebee641fdb32d5c12e1d25d90
CollyneJumah <collynejumah2010@gmail.com>
2022-08-20 13:46:20 +0300

    sidebar desgin

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -91,8 +91,8 @@ export const Navbar = () => {
                                 <AiOutlineCloseCircle size={25} className="text-[#86594c]" />
                             </div>
                         </div>
-                        <div className='border-b border-gray-300 my-4'>
-                            <p className='w-[85%] md:w-[90%] py-4 font-semibold text-center'>Build for the weekend. All articles in one dec.</p>
+                        <div className='border-b border-gray-300 my-1'>
+                            <p className='w-[85%] md:w-[90%] py-1 font-semibold text-center'>Build for the weekend. All articles in one dec.</p>
                         </div>
                     </div>
                     <div className='py-4 flex flex-col'>
