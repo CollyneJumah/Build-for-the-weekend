450d9babcc5aea853e692f9bdafb7065d4fa6cb7
CollyneJumah <collynejumah2010@gmail.com>
2022-08-24 22:39:02 +0300

    chore: wip on project page design 1. changing content to blog

--- props/ProjectItemsProps.jsx
+++ props/ProjectItemsProps.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const ProjectItemsProps = ({title,stack,backgroundImage,projectURL}) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+    <div className='relative flex items-center border-2 border-[#86594c] justify-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#86594c] to-[#493f3d]'>
         <Image className='rounded-xl group-hover:opacity-10' src={backgroundImage} alt="" />
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] '>
             <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
