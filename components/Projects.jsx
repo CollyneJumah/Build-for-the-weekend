a7de71448eedfd3f170b021f4f858234c864e2d6
CollyneJumah <collynejumah2010@gmail.com>
2022-10-17 10:51:54 +0300

    fix:removed the blog content part

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,7 +8,7 @@ export const Projects = () => {
         <div className="max-w-[1240px] mx-auto px-2 py-16">
             <p className='capitalize text-xl tracking-widest text-[#86594c] font-bold'>Blogs</p>
             <h2 className='py-4'>Give a read... 📑</h2>
-            <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-8">
+            {/* <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-8">
                 <ProjectItemsProps 
                     title="Tawi Health Platform"
                     backgroundImage={tawiImage}
@@ -33,7 +33,7 @@ export const Projects = () => {
                     projectURL= "/projects/cendalyne"
                     stack="React,Laravel"
                 />
-            </div>
+            </div> */}
         </div>
     </div>
   )
